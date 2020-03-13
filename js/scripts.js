8f65551e1f0c1fcf61f826aaed95443c941b7b7d
Matheus L. Donangelo <mldonangelo@gmail.com>
2020-03-13 07:16:06 -0300

    wyp: js

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+function showKrs(data, elem){
+  var krElement = document.getElementById(data);
+  krElement.classList.toggle('showKr');
+  elem.textContet = 'oi';
+  
+  console.log(elem.textContet);
+}
+
+function quarterOkrs(data, elem){
+  var links = document.querySelectorAll('.temporal-line a');
+  for(var i = 0; i < links.length; i++){
+    links[i].classList.remove('active');
+  }
+
+  elem.classList.add('active');
+
+  var allQuarterElements = document.querySelectorAll('.quarter-okr-content');
+  for (var i = 0; i < allQuarterElements.length; i++) {
+    allQuarterElements[i].classList.add('hidden');
+    allQuarterElements[i].classList.remove('show');
+  }
+
+  var quarterElement = document.getElementById(data);
+
+  quarterElement.classList.remove('hidden');
+  quarterElement.classList.add('show');
+}
